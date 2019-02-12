d3a0e1977391af412708e632564b474956716b94
Euan <euan_forrester@hotmail.com>
2019-02-11 17:35:00 -0800

    Successfully make our first Flickr API call

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ Proof of concept for a system that recommends photos based on previous photos yo
 
 # Instructions
 
-First make sure you have python installed
+First make sure you have python and some packages installed
 
 `brew install python`
+`pip3 install flickrapi`
 
 Then edit `config/config.ini` and `config/secrets.ini` to contain your Flickr API key and secret
+
+And in `config/config.ini` you can replace the Flickr user ID with your own. To get your numerical Flickr user ID, you may need to visit: http://idgettr.com/
--- recommend.py
+++ recommend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import configparser
+import flickrapi
 
 ENVIRONMENT = "prod"
 
@@ -9,5 +10,12 @@ config = configparser.ConfigParser()
 config.read("config/config.ini")
 config.read("config/secrets.ini")
 
-print(config[ENVIRONMENT]["flickr.api.key"])
-print(config[ENVIRONMENT]["flickr.api.secret"])
+flickrApiKey = config[ENVIRONMENT]["flickr.api.key"]
+flickrApiSecret = config[ENVIRONMENT]["flickr.api.secret"]
+flickrUserId = config[ENVIRONMENT]["flickr.user.id"]
+
+flickr = flickrapi.FlickrAPI(flickrApiKey, flickrApiSecret, format='parsed-json', cache=True)
+sets   = flickr.photosets.getList(user_id=flickrUserId)
+title  = sets['photosets']['photoset'][0]['title']['_content']
+
+print("Title of first set: %s" % (title))
