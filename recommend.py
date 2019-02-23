3bebc8b9d6ff27a9240ea357dacc8c52612e9c84
Euan Forrester <37133132+euan-forrester@users.noreply.github.com>
2019-02-23 00:25:59 -0800

    Add list of neighbors + links (#4)
    * Add link to each photo
    
    * Add in list of other users you may like
    
    * Make list of other users prettier by calling person.getInfo() to get their buddy icon and name

--- recommend.py
+++ recommend.py
@@ -22,7 +22,8 @@ flickr_api_retries                  = config.getint(ENVIRONMENT, "flickr.api.ret
 flickr_api_max_favorites_per_call   = config.getint(ENVIRONMENT, "flickr.api.favorites.maxpercall")
 flickr_api_max_favorites_to_get     = config.getint(ENVIRONMENT, "flickr.api.favorites.maxtoget")
 flickr_user_id                      = config.get(ENVIRONMENT, "flickr.user.id")
-num_results                         = config.getint(ENVIRONMENT, "results.num")
+num_photo_results                   = config.getint(ENVIRONMENT, "results.numphotos")
+num_neighbour_results               = config.getint(ENVIRONMENT, "results.numneighbours")
 memcached_location                  = config.get(ENVIRONMENT, "memcached.location")
 memcached_ttl                       = config.getint(ENVIRONMENT, "memcached.ttl")
 
@@ -43,6 +44,40 @@ settings.configure(CACHES = {
         'TIMEOUT': memcached_ttl,
     }
 })
+
+def get_person_info(flickr, user_id, max_retries):
+    num_retries = 0
+    person_info = None
+    success = False
+    error = None
+
+    while (num_retries < max_retries) and not success:
+        try:
+            person_info = flickr.people.getInfo(user_id=user_id)
+            success = True
+
+        except flickrapi.exceptions.FlickrError as e:
+            # You get random 502s when making lots of calls to this API, which apparently indicate rate limiting: 
+            # https://www.flickr.com/groups/51035612836@N01/discuss/72157646430151464/ 
+            # Sleeping between calls didn't seem to always solve it, but retrying does
+            # There doesn't seem to be a way to determine that this happened from the exception object other than to test
+            # the string against "do_request: Status code 502 received"
+            logging.debug("Got FlickrError %s" % (e))
+            error = e
+
+        except requests.exceptions.ConnectionError as e:
+            logging.debug("Got ConnectionError %s" % (e))
+            # Sometimes we see a random "Remote end closed connection without response" error
+            error = e
+
+        num_retries += 1
+
+    if not success:
+        raise error
+
+    logging.info("Just called get_person_info for user %s" % (user_id))
+
+    return person_info
 
 def get_favorites_page(flickr, user_id, max_retries, max_per_call, page_number):
     num_retries = 0
@@ -155,7 +190,7 @@ for neighbor_id in my_neighbors:
         logging.debug("Found neighbor favourite photo ", photo)
 
         my_neighbors[neighbor_id]['favorite_ids'].add(photo['id'])
-        all_neighbor_favorite_photo_ids[photo['id']] = { 'score': 0, 'url': photo.get('url_l', photo.get('url_m', '')) }
+        all_neighbor_favorite_photo_ids[photo['id']] = { 'score': 0, 'image_url': photo.get('url_l', photo.get('url_m', '')), 'id': photo['id'], 'user': photo['owner'] }
 
 # Now we can get the total number of favorites for each neighbor, as well as the number of favorites in common with us
 
@@ -181,12 +216,38 @@ sorted_neighbor_favorite_photo_ids = sorted(all_neighbor_favorite_photo_ids.item
 
 # The result is a list of tuples where the first element is the photo ID, and the second element is a dictionary of score and url
 
+# Similarly, we sort our neighbors themselves by their score
+
+sorted_neighbors = sorted(my_neighbors.items(), key=lambda x: x[1]['score'], reverse=True)
+
+# Get some info about each of our top users
+
+for neighbor in sorted_neighbors[0:num_neighbour_results]:
+    neighbor_info = get_person_info(flickr, neighbor[1]['user_id'], flickr_api_retries)
+    neighbor[1]['name'] = neighbor_info['person']['username']['_content']
+    neighbor[1]['photostream_url'] = neighbor_info['person']['photosurl']['_content']
+    neighbor[1]['favorites_url'] = '%sfavorites' % neighbor[1]['photostream_url']
+    
+    # Construct a link to their buddy icon according to these rules: https://www.flickr.com/services/api/misc.buddyicons.html
+    neighbor[1]['buddy_icon'] = 'https://www.flickr.com/images/buddyicon.gif'
+
+    icon_server = int(neighbor_info['person']['iconserver'])
+    if icon_server > 0:
+        neighbor[1]['buddy_icon'] = 'http://farm%d.staticflickr.com/%d/buddyicons/%s.jpg' % (neighbor_info['person']['iconfarm'], icon_server, neighbor_info['person']['nsid'])
+    
 # Write out our final file
 
 f = open(args.output_filename, "w+")
 
-for photo in sorted_neighbor_favorite_photo_ids[0:num_results]:
-    f.write("<img src=%s/><br/>" % (photo[1]['url']))
+f.write("<h1>Photos you may like</h1>\n")
+
+for photo in sorted_neighbor_favorite_photo_ids[0:num_photo_results]:
+    f.write("<a href=https://www.flickr.com/photos/%s/%s/><img src=%s/></a><br/>\n" % (photo[1]['user'], photo[1]['id'], photo[1]['image_url']))
+
+f.write("<h1>Other users you may like</h1>\n")
+
+for neighbor in sorted_neighbors[0:num_neighbour_results]:
+    f.write("<img src=%s/>%s - <a href=%s>Photos</a> - <a href=%s>Favorites</a><br/>\n" % (neighbor[1]['buddy_icon'], neighbor[1]['name'], neighbor[1]['photostream_url'], neighbor[1]['favorites_url']))
 
 f.close()
 
